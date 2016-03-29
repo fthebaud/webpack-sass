84000659c60fd2274172befab2780df1e27fe20a
Fabien Thebaud <fthebaud@gmail.com>
2016-03-29 16:25:29 +0200

    adding css extraction, webpack dev server and sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 // plugins dependencies
 var CleanWebpackPlugin = require('clean-webpack-plugin');
-// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // configuration object
@@ -29,12 +29,12 @@ var webpackConfig = {
   },
   output: {
     path: cfg.buildDir,
-    filename: 'css/[name]-compiled-[hash].js'
+    filename: 'js/bundle-[hash].js'
   },
   module: {
     loaders: [{
       test: /\.scss$/,
-      loader: "style-loader!css-loader!sass-loader"
+      loader: ExtractTextPlugin.extract("style-loader", "css-loader?sourceMap!sass-loader")
     }]
   },
   plugins: [
@@ -43,11 +43,20 @@ var webpackConfig = {
       verbose: true,
       dry: false
     }),
-    //new ExtractTextPlugin('resources/[name]-bundle-[hash].css'),
+    new ExtractTextPlugin('css/[name]-[hash].css'),
     new HtmlWebpackPlugin({
       template: cfg.srcDir + '/index.html'
     }),
-  ]
+  ],
+  /**DEV SERVER STUFF**/
+  // TODO: auto refresh, rendre debug env dependant, lire la doc web pack dev server + error lors du lancement...
+  debug: true,
+  devtool: "sourcemap", //for css sourcemaps, we need this option and the sourceMap option for the cssLoader
+  devServer: {
+    "port": 9090,
+    "contentBase": "build",
+    "inline": true
+  }
 };
 
 module.exports = webpackConfig;
